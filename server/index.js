26ded7fc0708ab4bbf734f1846ba1eb375f4f3ef
Ashritha Reddy Annadi <annadiashritha.an@gmail.com>
2026-03-27 11:44:54 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// server/index.js
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
+
+app.listen(6000, () => {
+  console.log("Server running on port 6000");
+});
